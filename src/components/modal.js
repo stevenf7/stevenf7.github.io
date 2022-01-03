5c85f55f0cfe7e9efd12c2696c017ab7b11a3888
Steven Feng <stevenfeng7@gmail.com>
2022-01-03 16:58:17 -0700

    finalized modal

--- src/components/modal.js
+++ src/components/modal.js
@@ -15,14 +15,22 @@ export default function Modal({ closeModal, id }) {
               closeModal(false)
             }}
           >
-            X
+          <h3>&#215;</h3>
           </button>
         </div>
         <div className="title">
           <h1>{data.projects[id].title}</h1>
+          <h2>{data.projects[id].date}</h2>
         </div>
         <div className="body">
-          <p>{data.projects[id].description}</p>
+        <img src={data.projects[id].workImg} alt="" className="img-fluid" />
+
+        <ul>
+        {data.projects[id].description.map((text, index) => (
+                <li>{text}</li>
+              ))}
+          
+          </ul>
         </div>
         {/* <div className="footer">
           <button onClick={() => closeModal(false)}>close</button>
