d9209cff5068fa6b3f26482b2c7d1e5b0a33fc53
stevenf7 <34409734+stevenf7@users.noreply.github.com>
2025-04-15 23:46:04 -0700

    footer adjustment, simplify nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,12 +12,12 @@ const Navbar = () => {
         </button>
           <div className="links-wrapper">
             <button onClick= {event =>  window.location.href='/#home'} type="button">Home</button>
-            <button onClick= {event =>  window.location.href='/#about'} type="button">About</button>
+            {/* <button onClick= {event =>  window.location.href='/#about'} type="button">About</button> */}
             <button onClick= {event =>  window.location.href='/#nvidia'} type="button">Work</button>
-            <button onClick= {event =>  window.location.href='/#education'} type="button">Education</button>
-            <button onClick= {event =>  window.location.href='/#work'} type="button">Internships</button>
+            {/* <button onClick= {event =>  window.location.href='/#education'} type="button">Education</button> */}
+            {/* <button onClick= {event =>  window.location.href='/#work'} type="button">Internships</button> */}
             <button onClick= {event =>  window.location.href='/#projects'} type="button">Projects</button>
-            <button onClick= {event =>  window.location.href='/#skills'} type="button">Skills</button>
+            {/* <button onClick= {event =>  window.location.href='/#skills'} type="button">Skills</button> */}
             <button onClick= {event =>  window.location.href='/#contact'} type="button">Contact</button>
           </div>
         </div>
