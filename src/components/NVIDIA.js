56b7a0b5b0c614f7b6ddcc5e68f33534f0deb302
Steven Feng <stevenfeng7@gmail.com>
2025-04-17 22:16:50 -0700

    videos

--- src/components/NVIDIA.js
+++ src/components/NVIDIA.js
@@ -6,12 +6,15 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "../styles/NVIDIA.scss"
 
 // Import NVIDIA images from the nvidia folder
-import h1Gif from "./../images/nvidia/h1.gif"
-import h1FlipGif from "./../images/nvidia/h1_flip.gif"
-// import img2309 from "./../images/nvidia/IMG_2309.jpg"
+import h1Gif from "./../images/nvidia/h1.mp4"
+import h1FlipGif from "./../images/nvidia/h1_Flip.mp4"
+import leatherbackVideo from "./../images/nvidia/Leatherback.mp4"
+import h1TrainVideo from "./../images/nvidia/h1_Train.mp4"
+import frankaMoveitVideo from "./../images/nvidia/Franka Moveit.mp4"
+import frankaDrawerVideo from "./../images/nvidia/Franka Drawer.mp4"
+import carterOutdoorVideo from "./../images/nvidia/Carter Outdoor.mp4"
+import agilityWalkVideo from "./../images/nvidia/Agility Walk.mp4"
 import gtc_lousd from "./../images/nvidia/IMG_2228.jpg"
-import img1667 from "./../images/nvidia/IMG_1667.jpg"
-import isaacSim from "./../images/nvidia/IsaacSim.jpg"
 import gtc_sil from "./../images/nvidia/gtc_sil.jpg"
 import newton from "./../images/nvidia/newton.jpg"
 
@@ -21,44 +24,70 @@ const NVIDIA = () => {
   // Create an array of carousel items with descriptions
   const carouselItems = [
     {
-      image: h1FlipGif,
-      title: "H1 Backflip",
+      media: h1FlipGif,
+      type: 'video',
+      title: "Humanoid robot tries to backflip",
       description: "H1 attempted a backflip, but it didn't go as planned."
     },
     {
-      image: h1Gif,
-      title: "H1 Locomotion Policy",
+      media: h1TrainVideo,
+      type: 'video',
+      title: "Training humanoids to walk",
+      description: "H1 robot during training phase."
+    },
+    {
+      media: h1Gif,
+      type: 'video',
+      title: "humanoid robot walking",
       description: "H1 is able to walk around using a reinforcement learning policy trained from Isaac Lab."
     },
     {
-      image: gtc_lousd,
+      media: frankaMoveitVideo,
+      type: 'video',
+      title: "Manipulator simulation",
+      description: "Franka robot demonstrating MoveIt integration."
+    },
+    {
+      media: frankaDrawerVideo,
+      type: 'video',
+      title: "Opening a drawer with a robot",
+      description: "Franka robot performing drawer manipulation task."
+    },
+    {
+      media: leatherbackVideo,
+      type: 'video',
+      title: "RC car simulation",
+      description: "RC car simulation"
+    },
+    {
+      media: carterOutdoorVideo,
+      type: 'video',
+      title: "Robot outdoor simulation",
+      description: "Robot outdoor simulation"
+    },
+    {
+      media: agilityWalkVideo,
+      type: 'video',
+      title: "Humanoid Robot Walking",
+      description: "Agility robot demonstrating walking capabilities."
+    },
+    {
+      media: gtc_lousd,
+      type: 'image',
       title: "Robot Composition talk at GTC 2025",
       description: "Giving a lecture at GTC 2025 on USD composition."
     },
     {
-      image: gtc_sil,
+      media: gtc_sil,
+      type: 'image',
       title: "ROS training lab at GTC 2025",
       description: "Giving a lecture at GTC 2025 on ROS integration with Isaac Sim."
     },
-    // {
-    //   image: img2309,
-    //   title: "GTC connect with expert, Robotics",
-    //   description: "Chatting with the robotics experts on topics from plants to rocket simulation using Isaac Sim"
-    // },
     {
-      image: img1667,
-      title: "GTC connect with expert, Digital Twins",
-      description: "Chatting with the digital twins experts simulating a whole AI factory using Omniverse"
-    },
-    {
-      image: newton,
+      media: newton,
+      type: 'image',
       title: "Presenting the Newton Simulator at GTC 2025",
       description: "Presenting Newton, next generation robotics simulator at our both at NVIDIA GTC 2025."
-    },
-    {
-      image: isaacSim,
-      title: "A1 quadruped locomotion simulation",
-      description: "Simulating A1 quadruped robot in Isaac Sim."
     }
   ];
 
@@ -86,12 +115,23 @@ const NVIDIA = () => {
             >
               {carouselItems.map((item, index) => (
                 <Carousel.Item key={index}>
-                  <img
-                    className="d-block w-100"
-                    src={item.image}
-                    alt={item.title}
-                    fluid
-                  />
+                  {item.type === 'video' ? (
+                    <video
+                      className="d-block w-100"
+                      src={item.media}
+                      autoPlay
+                      muted
+                      loop
+                      playsInline
+                    />
+                  ) : (
+                    <img
+                      className="d-block w-100"
+                      src={item.media}
+                      alt={item.title}
+                      fluid
+                    />
+                  )}
                   <Carousel.Caption className="carousel-caption">
                     <h3>{item.title}</h3>
                     {/* <p>{item.description}</p> */}
