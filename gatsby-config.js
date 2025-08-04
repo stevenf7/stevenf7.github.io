9a7d3f009eaff5a8087d9a66e19fc0f260fb8c05
Steven Feng <stevenfeng7@gmail.com>
2025-08-03 21:24:18 -0700

    google analytics config

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,15 @@ module.exports = {
     },
     `gatsby-plugin-sass`,
     `gatsby-plugin-smoothscroll`,
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // Use environment variable for security
+        trackingId: process.env.GATSBY_GA_MEASUREMENT_ID,
+        head: false,
+        anonymize: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
